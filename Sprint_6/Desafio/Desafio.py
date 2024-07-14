abfa5ef8ced86c9c540d5c945c573ffdc7a07524
Wendel <156434274+WendeldsCoelho@users.noreply.github.com>
2024-07-14 15:39:06 -0300

    update Desafio

--- Sprint_6/Desafio/Desafio.py
+++ Sprint_6/Desafio/Desafio.py
@@ -1,8 +1,10 @@
+# importar bibliotecas necessárias
 import os
 import boto3
 from datetime import datetime
 from dotenv import load_dotenv
 
+# Carregar as chaves da AWS
 load_dotenv('.env')
 
 # Configurações
@@ -10,10 +12,14 @@ bucket_name = 'desafio-sprint6-w'
 storage_layer = 'Raw'
 data_origin = 'Local'
 data_format = 'CSV'
-movies_file_path = '/app/data/movies.csv'
-series_file_path = '/app/data/series.csv'
 
-def upload_to_s3(file_path, data_specification):
+# Caminhos para os arquivos csv
+movies_file_path = '/app/data/movies.csv' 
+series_file_path = '/app/data/series.csv' 
+
+# Função para o upload de um arquivo para o S3
+def upload_s3(file_path, data_specification):
+    # Criação de um cliene S3 utilizando as credencias AWS
     s3_client = boto3.client(
         's3',
         aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
@@ -21,20 +27,26 @@ def upload_to_s3(file_path, data_specification):
         aws_session_token=os.getenv('AWS_SESSION_TOKEN'),
         region_name=os.getenv('AWS_DEFAULT_REGION')                 
                              )
-    file_name = os.path.basename(file_path)
+    # Obter o nome do arquivo
+    nome_Arquivo = os.path.basename(file_path)
+
+    # Obter a data atual no formata requisitado
     current_date = datetime.now().strftime('%Y/%m/%d')
 
-    s3_key = f"{storage_layer}/{data_origin}/{data_format}/{data_specification}/{current_date}/{file_name}"
+    # Criação do caminho no bucket
+    s3_key = f"{storage_layer}/{data_origin}/{data_format}/{data_specification}/{current_date}/{nome_Arquivo}"
 
+    # Upload do arquivo com tratamento de exceção
     try:
         s3_client.upload_file(file_path, bucket_name, s3_key)
         print(f"Upload bem-sucedido de {file_path} para s3://{bucket_name}/{s3_key}")
     except Exception as e:
         print(f"Erro ao fazer upload de {file_path}: {e}")
 
+# Função para executar os uploads
 def main():
-    upload_to_s3(movies_file_path, 'Movies')
-    upload_to_s3(series_file_path, 'Series')
+    upload_s3(movies_file_path, 'Movies')
+    upload_s3(series_file_path, 'Series')
 
 if __name__ == "__main__":
     main()
